d8bc8c670c3647879898133a87f50e5f8338a19a
Franklin Sumner <sumnerfranklinm@gmail.com>
2022-11-19 09:11:18 -0500

    added arrays for special characters, numbers, and letters, added character length min and max

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+var randomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var randomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", 
+"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+"u", "v", "w", "x", "y", "z"]
+
+var minCharacterLength = 8;
+var maxCharacterLength = 128;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
