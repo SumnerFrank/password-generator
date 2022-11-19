30176fd7d2bb9a69df1b413ca556218d1c8f5811
Franklin Sumner <sumnerfranklinm@gmail.com>
2022-11-19 10:28:37 -0500

    added functions for prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,16 @@
 // Assignment code here
 
-function generatePassword() {
-//prompt user for pw length and character types 
-//validate selections 
-//generate password 
-//display password 
-  
-
-}
-
-
 var randomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var randomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", 
 "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
 "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", 
+"I", "J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+"U", "V", "W", "X", "Y", "Z"]
 
-var minCharacterLength = 8;
-var maxCharacterLength = 128;
-
+var characterLength = 8;
+var selectedChoices = [];
 
 
 // Get references to the #generate element
@@ -31,6 +23,31 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  getPrompts
+}
+
+function getPrompts() {
+  characterLength = prompt("How many characters do you want your password to be? (8 - 128 characters")
+  if(characterLength < 8 || characterLength > 128){
+    alert("Character length should be between 8 and 128");
+    return false;
+  }
+  if(confirm("Would you like upper case letters in your password?")) {
+  selectedChoices = selectedChoices.concat(upperCase); 
+  }
+  if(confirm("Would you like lower case letters in your password?")) {
+  selectedChoices = selectedChoices.concat(lowerCase); 
+  }
+  if(confirm("Would you like numbers in your password?")) {
+  selectedChoices = selectedChoices.concat(randomNumber); 
+  }
+  if(confirm("Would you like special characters in your password?")) {
+  selectedChoices = selectedChoices.concat(randomSymbol); 
+  }
+  return true;
 }
 
 // Add event listener to generate button
